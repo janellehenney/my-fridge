80d3ef9832e7bbc0c332f799fe34d4238312c247
Janelle Henney <janellehenney@gmail.com>
2019-06-07 11:58:23 -0500

    up and running demo

---
+++ js/components/Navigator.js
@@ -0,0 +1,42 @@
+import { createStackNavigator } from 'react-navigation';
+import { HomeScreen } from './HomeScreen';
+import { DetailsScreen } from './DetailsScreen';
+import { ModalScreen } from './ModalScreen';
+
+const MainStack = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+
+export const RootStack = createStackNavigator(
+  {
+    Main: {
+      screen: MainStack,
+    },
+    MyModal: {
+      screen: ModalScreen,
+    },
+  },
+  {
+    mode: 'modal',
+    headerMode: 'none',
+  }
+);
